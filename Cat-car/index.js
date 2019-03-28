18e178986a6e38c142d8ef125b8aa30bf8d9f3ab
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-03-28 14:03:38 -0500

    add cat-carosal answer

--- Cat-car/index.js
+++ Cat-car/index.js
@@ -1,12 +1,16 @@
 /* eslint-disable indent */
 /* eslint-disable strict */
 /* global $ */
+
+
 function main () {
-    $('.thumbnails').on('click', event => {
-        const targetCat = $(event.currentTarget);
-        $('hero').html(targetCat.html());
-        console.log(targetCat.html());
-       
+    $('.thumbnail').on('click', function (event) {
+        const imgSrc = $(this).find('img').attr('src');
+        const imgAlt = $(this).find('img').attr('alt');
+        
+        $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+
+        // $('hero img').attr({'src' : imgSrc}, {'alt' : imgAlt});
     });
 }
  $(main);
